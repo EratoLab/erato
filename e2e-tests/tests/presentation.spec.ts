f05af44bcb94834df95907e1eb94f577413605ce
Max Goisser <max@goisser.com>
2025-06-11 11:22:51 +0200

    Fix locale detection if browser provides long language tag

--- e2e-tests/tests/presentation.spec.ts
+++ e2e-tests/tests/presentation.spec.ts
@@ -148,3 +148,29 @@ test(
     await expectIsDarkPage(page, "Page should stay dark after reload");
   },
 );
+
+test(
+  "Can login and see german language by default",
+  { tag: TAG_CI },
+  async ({ browser }) => {
+    const context = await browser.newContext({
+      locale: "de-DE",
+    });
+    const page = await context.newPage();
+    await page.goto("/");
+
+    await page.getByRole("button", { name: "Sign in with" }).click();
+    await page.waitForURL((url) => url.pathname.includes("auth"));
+    // await page.getByRole('textbox', { name: 'email address' }).click();
+    await page
+      .getByRole("textbox", { name: "email address" })
+      .fill("admin@example.com");
+    await page.getByRole("textbox", { name: "Password" }).fill("admin");
+    await page.getByRole("textbox", { name: "Password" }).press("Enter");
+    await page.getByRole("button", { name: "Grant Access" }).click();
+
+    await expect(
+      page.getByRole("textbox", { name: "Nachricht eingeben..." }),
+    ).toBeVisible();
+  },
+);
