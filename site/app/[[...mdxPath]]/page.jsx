f9e250ee1e02af7f7fb207ddf8ba23eb3afab584
Max Goisser <max@goisser.com>
2025-07-13 22:40:40 +0200

    Adjust site repository link; Add suffix to page titles

--- site/app/[[...mdxPath]]/page.jsx
+++ site/app/[[...mdxPath]]/page.jsx
@@ -6,7 +6,10 @@ export const generateStaticParams = generateStaticParamsFor("mdxPath");
 export async function generateMetadata(props) {
   const params = await props.params;
   const { metadata } = await importPage(params.mdxPath);
-  return metadata;
+  return {
+    ...metadata,
+    title: `${metadata.title} - Erato`,
+  };
 }
 
 const Wrapper = getMDXComponents().wrapper;
