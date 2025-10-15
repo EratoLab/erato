f4a925efda8bd6201ffc6967bd5f6a86e3f24887
Max Goisser <max@goisser.com>
2025-10-15 11:24:13 +0200

    Reduce size of "big" file from 100MB to 20MB

--- e2e-tests/scripts/generate_test_files.sh
+++ e2e-tests/scripts/generate_test_files.sh
@@ -2,5 +2,5 @@
 set -euxo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-"$SCRIPT_DIR/generate_large_pdf_binary.py" --size 100 "$SCRIPT_DIR/../test-files/big-file-100mb.pdf"
+"$SCRIPT_DIR/generate_large_pdf_binary.py" --size 20 "$SCRIPT_DIR/../test-files/big-file-20mb.pdf"
 "$SCRIPT_DIR/generate_lorem_pdf.py" --words 100000 "$SCRIPT_DIR/../test-files/long-file-100k-words.pdf"
