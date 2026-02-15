543b5460f9fa1628af0d324fcdb2070f10a0685c
Max Goisser <max@goisser.com>
2026-02-15 14:30:05 +0100

    Fix missing e2e test files generation

--- e2e-tests/scripts/generate_test_files.sh
+++ e2e-tests/scripts/generate_test_files.sh
@@ -2,5 +2,13 @@
 set -euxo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-"$SCRIPT_DIR/generate_large_pdf_binary.py" --size 20 "$SCRIPT_DIR/../test-files/big-file-20mb.pdf"
-"$SCRIPT_DIR/generate_lorem_pdf.py" --words 100000 "$SCRIPT_DIR/../test-files/long-file-100k-words.pdf"
+TEST_FILES_DIR="$SCRIPT_DIR/../test-files"
+
+mkdir -p "$TEST_FILES_DIR"
+
+"$SCRIPT_DIR/generate_large_pdf_binary.py" --size 20 "$TEST_FILES_DIR/big-file-20mb.pdf"
+"$SCRIPT_DIR/generate_lorem_pdf.py" --words 100000 "$TEST_FILES_DIR/long-file-100k-words.pdf"
+"$SCRIPT_DIR/generate_multipage_test_pdf.py" "$TEST_FILES_DIR/multipage-test.pdf" 20
+"$SCRIPT_DIR/generate_corrupted_pdf.py" "$TEST_FILES_DIR/corrupted_truncated.pdf" truncated
+"$SCRIPT_DIR/generate_corrupted_pdf.py" "$TEST_FILES_DIR/corrupted_malformed.pdf" malformed_stream
+"$SCRIPT_DIR/generate_corrupted_pdf.py" "$TEST_FILES_DIR/corrupted_unsupported_encryption.pdf" unsupported_encryption
