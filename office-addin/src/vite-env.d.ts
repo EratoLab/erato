6591edd88feecc4cb562da08c6f004f947d7ef1b
Daniel <4836770+bdart@users.noreply.github.com>
2026-04-12 15:31:16 +0200

    Fix missing dev env config loading (#608)

--- office-addin/src/vite-env.d.ts
+++ office-addin/src/vite-env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="vite/client" />
 /// <reference types="office-js" />
+
+interface ImportMetaEnv {
+  readonly VITE_MSAL_CLIENT_ID?: string;
+  readonly VITE_MSAL_AUTHORITY?: string;
+}
